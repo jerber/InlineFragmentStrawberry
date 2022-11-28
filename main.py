0b2c0c5eb4b5ea6985b774262db57773f10a7ecf
Jeremy Berman <jerber@sas.upenn.edu>
2022-11-28 18:11:31 +0000

    first commit

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import strawberry
 
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
+from strawberry.types import Info
+
+from devtools import debug
+
+
+@strawberry.type
+class Missing:
+    message: str
+
+
+@strawberry.type
+class User:
+    id: str
+    name: str
 
 
 @strawberry.type
@@ -10,6 +24,13 @@ class Query:
     @strawberry.field
     def hello(self) -> str:
         return "Hello World"
+
+    @strawberry.field
+    def user(self, info: Info) -> User | Missing:
+        headers = info.context['request'].headers
+        if headers.get('user') == '1':
+            return User(id="1", name="Patrick Star")
+        return Missing(message="User not found")
 
 
 schema = strawberry.Schema(Query)
